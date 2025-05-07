19a87ff4f01b299e9243c57eccae655c36f87380
Javid Ahmed <javidahmed@icloud.com>
2025-05-07 19:50:28 +0100

    Initial commit of WorkerNPC mod files including solution, project, assembly info, and main plugin code.

---
+++ WorkerNPC/WorkerNPC.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+
+namespace WorkerNPC
+{
+    [BepInPlugin(pluginGUID, pluginName, pluginVersion)]
+    public class Main : BaseUnityPlugin
+    {
+        const string pluginGUID = "com.example.GUID";
+        const string pluginName = "WorkerNPC";
+        const string pluginVersion = "0.1.0";
+
+        private readonly Harmony HarmonyInstance = new Harmony(pluginGUID);
+
+        public static ManualLogSource logger = BepInEx.Logging.Logger.CreateLogSource(pluginName);
+
+        public void Awake()
+        {
+            Main.logger.LogInfo("Loading WorkerNPC mod...");
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            HarmonyInstance.PatchAll(assembly);
+        }
+    }
+}
