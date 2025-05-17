bba620f3f8f5c09117ff7a2a8a623d64674f36a2
Javid Ahmed <javidahmed@icloud.com>
2025-05-17 22:57:07 +0100

    Add BuildingRepairWorker and BuildingRepairWorkerBehaviour classes for building repair functionality

---
+++ WorkerNPC/npc/BuildingRepairWorker.cs
@@ -0,0 +1,47 @@
+﻿using Jotunn;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace WorkerNPC
+{
+    internal class BuildingRepairWorker : WorkerNPCBase
+    {
+        static string customName = BuildingRepairWorkerConfig.customName;
+
+        internal override GameObject GetBaseNPC()
+        {
+            GameObject clonedNPC = base.GetBaseNPC();
+            clonedNPC.name = customName;
+            clonedNPC.AddComponent<BuildingRepairWorkerBehaviour>();
+            return clonedNPC;
+        }
+    }
+
+    internal class BuildingRepairWorkerBehaviour : NPCBehaviour
+    {
+        float searchRadius = BuildingRepairWorkerConfig.searchRadius;
+
+        internal new void Start()
+        {
+            base.Start();
+            jobInterval = BuildingRepairWorkerConfig.jobInterval;
+        }
+
+        internal override void DoJob()
+        {
+            base.DoJob();
+
+            WearNTear[] buildings = FindNearbyResource<WearNTear>(searchRadius);
+
+            foreach (WearNTear building in buildings)
+            {
+                if (building.GetHealthPercentage() < 1f)
+                {
+                    MoveTo(building.transform.position);
+                    building.Repair();
+                }
+            }
+        }
+    }
+}
